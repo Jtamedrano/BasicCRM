8de01584965ebe3cafa1a6ffbf19e510d99d7709
Jesse Medrano <Jtamedrano@gmail.com>
2020-11-18 21:14:54 -0800

    init repo

--- basic_crm/src/App.js
+++ basic_crm/src/App.js
@@ -1,7 +1,12 @@
+import styled from "styled-components";
+import Home from "./views/Home";
+
 function App() {
   return (
     <>
-      <div className="App"></div>
+      <div className="App">
+        <Home />
+      </div>
     </>
   );
 }
